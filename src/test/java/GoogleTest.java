654c29d56073ab4901465ad2c4a9ca3351957483
Sirozhiddin Zhumabaev <szhumabaev@fun.co>
2021-02-15 20:27:41 +0300

    Initial commit

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleTest {
+
+    @Test
+    void selenideSearchTest() {
+      // Открыть google
+      open("https://www.google.com/");
+
+      // Ввести Selenide в поиск
+      $(byName("q")).setValue("Selenide").pressEnter();
+
+      // Проверить, что Selenide появился в результатах поиска
+      $("#search").shouldHave(text("selenide.org"));
+    }
+}
